3dff56776cec568218828e78c644dc497c242b76
yube01 <yubraja008@gmail.com>
2024-12-02 19:54:37 +0545

    signout added

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,21 +1,87 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+
+interface Post {
+    fullName: string;
+    avatarLink: string;
+}
 
 const Navbar: React.FC = () => {
+
+    const [user, setUser] = useState<boolean>(false)
+    const [posts, setPosts] = useState<Post | null>(null);
+    const navigate = useNavigate()
+
+
+    const handleSignOut = ()=>{
+        localStorage.removeItem("userToken");
+        navigate("/signin")
+
+    }
+
+
+
+    useEffect(() => {
+        const userId = localStorage.getItem("userToken");
+        if (userId) {
+            setUser(true);
+            const handledata = async () => {
+                try {
+                    const response = await fetch(`${import.meta.env.VITE_API}/user/singleUser/${userId}`, {
+                        method: "GET"
+                    })
+
+                    const data = await response.json();
+                    setPosts(data.user[0]);
+
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+            handledata()
+        }
+    }, [])
+
     return (
-        <div className=' text-white flex gap-4 justify-between text-2xl'>
+        <div className=' text-white flex justify-between text-2xl py-2 sm:py-6'>
             <div className='space-x-4 sm:space-x-10'>
                 <Link className=' link-style' to="/">Home</Link>
                 <Link className=' link-style' to="/profile">Profile</Link>
             </div>
-            <div className=' flex space-x-4 sm:space-x-10'>
-                <Link className=' link-style' to="/signin">
-                    <Button className=' bg-white text-black hover:text-white'>Sign In</Button>
-                </Link>
-                <Link className=' link-style' to="/signup">
-                    <Button className=' bg-white text-black hover:text-white'>Sign Up</Button>
-                </Link>
+            <div className=' flex items-center justify-center space-x-4 sm:space-x-6'>
+                {
+                    !user ?
+                        <>
+                            <Link className=' link-style' to="/signin">
+                                <Button className=' bg-white text-black hover:text-white'>Sign In</Button>
+                            </Link>
+                            <Link className=' link-style' to="/signup">
+                                <Button className=' bg-white text-black hover:text-white'>Sign Up</Button>
+                            </Link>
+                        </> :
+                        <>
+
+                           {
+                            posts ? <>
+                             <Avatar className="h-8 w-8">
+                                <AvatarImage src={posts.avatarLink} alt="" />
+                                <AvatarFallback className="text-sm">""</AvatarFallback>
+                            </Avatar>
+                            </>:<>
+                            <Avatar className="h-8 w-8">
+                                <AvatarImage src="" alt="" />
+                                <AvatarFallback className="text-sm">Y</AvatarFallback>
+                            </Avatar></>
+                           }
+                            <Button 
+                            onClick={handleSignOut}
+                            className=' bg-white text-black hover:text-white'>Sign Out</Button>
+
+                        </>
+                }
             </div>
         </div>
     )
