ca6bf319a44a888a78248858962ed29f641f0b66
yube01 <yubraja008@gmail.com>
2024-12-02 16:29:02 +0545

    profile feature added

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,100 @@
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+interface User {
+  id: number;
+  fullName: string;
+  avatarLink: string;
+}
+
+
 const Profile = () => {
-    return (
-      <div>Profile</div>
-    )
+  const [users, setUsers] = useState<User[]>([]);
+
+  const handleData = async () => {
+
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API}/user/allUser`, {
+        method: "GET"
+      })
+
+      const data = await response.json();
+      setUsers(data.users);
+      console.log(data)
+
+
+    } catch (error) {
+      console.log(error)
+    }
   }
-  
-  export default Profile
+
+  useEffect(() => {
+    handleData()
+  }, [])
+
+  return (
+    <div className="flex text-white justify-center w-[100vw] bg-black min-h-[100vh]">
+      <div className="w-full p-5 lg:w-[60rem] mr-3 py-10 space-y-5">
+        <div className=' my-2 md:my-16 flex items-center justify-between'>
+          <h1 className='  text-[2rem] md:text-[3.5rem] text-white'>
+            User
+          </h1>
+
+        </div>
+        <Table className=" text-sm sm:text-xl">
+          <TableHeader>
+            <TableRow >
+              <TableHead className=" w-max sm:[w-20rem] md:w-[25rem] lg:w-[35rem] text-white">Full Name</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody className=" h-auto">
+            {
+              (users.length > 0 ? (
+                users.map((user) => (
+                  <TableRow key={user.id}>
+                    <Link
+                      className="link-style"
+                      to={`/posts/${user.fullName}`}
+                      state={{ authorId: user.id, author: user.fullName }}>
+                      <TableCell className=" font-light flex items-center gap-2">
+                        <Avatar className="h-6 w-6">
+                          <AvatarImage src={user.avatarLink} alt="" />
+                          <AvatarFallback className="text-sm bg-black font-bold">{user.fullName.charAt(0).toUpperCase()}</AvatarFallback>
+                        </Avatar>
+
+                        {user.fullName}
+                      </TableCell>
+                    </Link>
+
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={4} className="text-center font-medium">
+                    No posts available
+                  </TableCell>
+                </TableRow>
+              ))
+
+            }
+
+          </TableBody>
+
+        </Table>
+
+
+      </div>
+    </div>)
+}
+
+export default Profile
--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,17 +1,19 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar: React.FC = () => {
     return (
-        <div className=' text-white flex justify-between text-2xl'>
-            <div className=' space-x-10'>
+        <div className=' text-white flex gap-4 justify-between text-2xl'>
+            <div className='space-x-4 sm:space-x-10'>
                 <Link className=' link-style' to="/">Home</Link>
                 <Link className=' link-style' to="/profile">Profile</Link>
             </div>
-            <div className=' space-x-10'>
-                <Link className=' link-style'
-                    to="/signin">Sign In</Link>
-                <Link className=' link-style' to="/signup">Sign Up</Link>
+            <div className='space-x-4 sm:space-x-10'>
+                <Link className=' link-style' to="/signin">
+                    <Button className=' bg-white text-black hover:text-white'>Sign In</Button></Link>
+                <Link className=' link-style' to="/signup">
+                <Button className=' bg-white text-black hover:text-white'>Sign Up</Button></Link>
             </div>
         </div>
     )
